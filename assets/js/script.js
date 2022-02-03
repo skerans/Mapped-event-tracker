e598dea28d93b9e9fcf04ab09dfffb456eaf92b4
Spencer Kerans <skerans0@gmail.com>
2022-02-02 21:04:24 -0700

    added js to check eonet api

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,1 +1,21 @@
 console.log("hello world");
+
+
+$( document ).ready(function() {
+  $.getJSON( "https://eonet.sci.gsfc.nasa.gov/api/v3/events", {
+      status: "open",
+      limit: 20
+  })
+  .done(function( data ) {
+      $.each( data.events, function( key, event ) {
+          $( "#eventList" ).append(
+              "<dt>" + event.id + ": " + event.title + "</dt>"
+          );
+          if (event.description != null &&event.description.length) {
+              $( "#eventList" ).append(
+                  "<dd><em>" + event.description + "</em></dd>"
+              );
+          }
+      });
+  });
+}); 
