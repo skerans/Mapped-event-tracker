77e08bb13517a311637b589804859bda81c3002a
Nick Martin <martin.nicholas.r@gmail.com>
2022-02-09 18:24:47 -0700

    pullingevents within boundaries

--- assets/js/script.js
+++ assets/js/script.js
@@ -15,133 +15,104 @@
 //search bar with clickable search button
 
 //hamburger menu
-   //two date pickers
-      //datepicker FROM
-      //datepicker TO
-   //checkboxes for Event Types
-      //API call to determine different event types available then dynamically create checkboxes
+//two date pickers
+//datepicker FROM
+//datepicker TO
+//checkboxes for Event Types
+//API call to determine different event types available then dynamically create checkboxes
 
 //help and about div “buttons” bottom right - clickable with modal popup?
 
 //Invisible DIV created for errors, will be set visible when error is needed
-   //div section left(small) is for error icon
-   //div section middle is for error text
-   //div section right(small)
+//div section left(small) is for error icon
+//div section middle is for error text
+//div section right(small)
 
 //when a user searches location
-   //convert to string
-   //use API to convert city to coordinates
-   //check if status returns OK - Error IF not.
-   //Pass Coordinates to Leaflet - refresh screen with new location
+//convert to string
+//use API to convert city to coordinates
+//check if status returns OK - Error IF not.
+//Pass Coordinates to Leaflet - refresh screen with new location
 
 //when user changes Date FROM
-   //Error check to see if FROM is earlier than TO
-   //Get new data based on new date range and map scope - REFRESH
+//Error check to see if FROM is earlier than TO
+//Get new data based on new date range and map scope - REFRESH
 //when user changes Date TO
-   //Error check to see if TO is earlier than FROM
-   //Get new data based on new date range  and map scope - REFRESH 
+//Error check to see if TO is earlier than FROM
+//Get new data based on new date range  and map scope - REFRESH 
 
 //when user selects a checkbox option
-   //Get new data based on current date range and map scope - REFRESH
+//Get new data based on current date range and map scope - REFRESH
 
 //when item is on map
-   //item hover shows the summary of event
+//item hover shows the summary of event
 
 console.log("Start of JS");
+
+///// GLOBAL VARIABLES /////
 let eventCount = 20;
 let searchBtn = document.getElementById("search-btn");
 let searchText = document.getElementById("search-city");
 let dataRefreshBtn = document.getElementById("data-refresh-btn");
 
 
+///// Creating the map /////
+var map = L.map('map').setView([39.85, -104.67], 10);
+L.tileLayer('https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png', {
+   maxZoom: 19,
+   attribution: '&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
+}).addTo(map);
+let layerGroup = L.layerGroup().addTo(map)
+let bounds = map.getBounds();
+
+let minLong = bounds.getWest();
+let maxLong = bounds.getEast();
+let minLat = bounds.getSouth();
+let maxLat = bounds.getNorth();
+
 //initial pull of data points from EONET
 dataPull();
 
 
 
-var map = L.map('map').setView([39.85, -104.67], 10);
-L.tileLayer('https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png', {
-	maxZoom: 19,
-	attribution: '&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
-}).addTo(map);
-let layerGroup = L.layerGroup().addTo(map)
+///// FUNCTIONS /////
 
+// This finction gets the boundaries of the current map view
+function getNewBoundaries() {
+   // Storing the map boundaries in a variable
+   bounds = map.getBounds();
 
-
-
-// NM - working on getting the boundaries of the map when moving or zooming...
-
-// This runs whenever the map stops moving
-// This signle event appears to capture both panning and zooming
-map.on('moveend', () => {
-
-   // Storing the bounds in variable
-   let bounds = map.getBounds();
-
-   // Console logging the bounds in its default state
-   // Which appears to be two arrays in an object
-   // One for the northeast corner, and the other for the southwest
-   console.log('---- NE and SW corners only ----')
-   console.log(bounds);
-
-   // Found a way to convert it to a single string
-   // which may be helpful if we are passing this off to an API as a parameter
-   console.log('---- NE and SW corners only, but all four values as one continuous string ----')
-   console.log(bounds.toBBoxString());
-
-   // now that bounds is set in a variable - can run the additional methods below
-
-   // This is how we can the other corners that aren't returned by the default getBounds()
-   console.log('---- lats and longs of the four corners of the viewport starting with NW and going clockwise ----')
-   console.log(bounds.getNorthWest());
-   console.log(bounds.getNorthEast());
-   console.log(bounds.getSouthEast());
-   console.log(bounds.getSouthWest());
-   
-   // This will return that lat/longs of the four borders of the viewport
-   console.log('---- lats and longs of the four sides of the viewport ----')
-   console.log(bounds.getNorth());
-   console.log(bounds.getSouth());
-   console.log(bounds.getEast());
-   console.log(bounds.getWest());
-});
-
- 
-
-function closeModal() {
-   $('.modal').addClass('hidden');
-   $('header, #map, main.overlay').removeClass('blur');
+   // setting the Lat/Long variables with the current boundaries of the four sides of the map
+   minLong = bounds.getWest();
+   maxLong = bounds.getEast();
+   minLat = bounds.getSouth();
+   maxLat = bounds.getNorth();
 }
 
-function openModal(evt) {
-   $('.modal').removeClass('hidden');
-   $('header, #map, main.overlay').addClass('blur');
 
-   let selectedModal = evt.target.getAttribute('data-modal');
-   
-   $('.modal-header h2').text(selectedModal);
-   };
-
-function dataPull(){
+// This function fetches event data from the EONET API and uses it to populate the event markers on the map
+function dataPull() {
    //query eonet API
-   let queryEONET = `https://eonet.sci.gsfc.nasa.gov/api/v3/events?limit=${eventCount}&status=open`;
+   let queryEONET = `https://eonet.sci.gsfc.nasa.gov/api/v3/events?bbox=${minLong},${maxLat},${maxLong},${minLat}&limit=${eventCount}&status=open`;
    fetch(queryEONET)
-   .then(response => response.json())
+      .then(response => response.json())
       .then(data => {
          let eventData = data.events;
          console.log(eventData);//DELETE LATER
          console.log(`eventdata length is ${eventData.length}`);//DELETE LATER
-   //add markers to map based on eventData length
+         //add markers to map based on eventData length
          for (let index = 0; index < eventData.length; index++) {
             var date = new Date(data.events[index].geometry[0].date);
-            L.marker([data.events[index].geometry[0].coordinates[1], data.events[index].geometry[0].coordinates[0]])
-            .addTo(map)
-            .bindPopup(`${data.events[index].title} -\n Date/Time: ${date.toString()}`); //marker description with date
-         }   
+            var eventMarker = L.marker([data.events[index].geometry[0].coordinates[1], data.events[index].geometry[0].coordinates[0]]);
+            eventMarker.addTo(layerGroup)
+               .bindPopup(`${data.events[index].title} -\n Date/Time: ${date.toString()}`); //marker description with date
+         }
       });
    console.log("API call complete");//DELETE later
 };
 
+
+// Getting the city coordinates based on user entry in the city search bar
 function getCityCoord(event) {
    event.preventDefault();
 
@@ -160,7 +131,7 @@ function getCityCoord(event) {
          .then(function (response) {
             if (response.ok) {
                response.json().then(function (data) {
-                  console.log(data); 
+                  console.log(data);
                   if (data.length > 0) {  // checks if the city found 
                      const checkCity = data[0].name;
                      console.log(checkCity);
@@ -175,25 +146,28 @@ function getCityCoord(event) {
                         console.log(lon);
 
                         L.marker([data[0].lat, data[0].lon])
-                          .addTo(layerGroup)
-                          .bindPopup(`${checkCity} - ${newCity}`); // add marker
+                           .addTo(layerGroup)
+                           .bindPopup(`${checkCity} - ${newCity}`); // add marker
                         map.setView([lat, lon], 10) //set map to location, zoom to 10
-                     } 
+                     }
                   } else {
                      alert("The city is not found!");
                   }
                });
             }
          });
-         // cc
    }
 };
 
-function dataRefresh(){
+// Data Refresh Function
+function dataRefresh() {
    console.log("getting and setting new variable options then calling dataPull");
+
    //clear all existing point
    layerGroup.clearLayers();
-   // dataPull();
+
+   // Pull new data
+   dataPull();
 };
 
 
@@ -208,7 +182,27 @@ function menuToggleHide() {
 };
 
 
+// Function to open the modals
+function openModal(evt) {
+   $('.modal').removeClass('hidden');
+   $('header, #map, main.overlay').addClass('blur');
+
+   let selectedModal = evt.target.getAttribute('data-modal');
+
+   $('.modal-header h2').text(selectedModal);
+};
+
+// Function to close the currently opened modal
+function closeModal() {
+   $('.modal').addClass('hidden');
+   $('header, #map, main.overlay').removeClass('blur');
+}
+
+
 ////// EVENT HANDLERS //////
+
+// Map move event -- triggers new boundaries
+map.on('moveend', getNewBoundaries);
 
 //Open Modal
 $('.modal-btn').on('click', function (evt) {
@@ -224,12 +218,15 @@ $('.modal-container').on('click', function (evt) {
    evt.stopPropagation();
 });
 
+// Search City Event
 $("#search-bar").on("submit", function (event) {
-getCityCoord(event);
-$("#search-city").val("");
+   getCityCoord(event);
+   $("#search-city").val("");
 });
 
+// Refresh Data Event
 dataRefreshBtn.addEventListener("click", dataRefresh);
+
 //Open Options Menu
 $('#menu-open-btn').on('click', menuToggleHide);
 
